68c95878398b5d3cc48650ad7dfb22e2289e567a
Shelly Cooper <shelly.cooper@wustl.edu>
2022-01-26 18:27:01 -0600

    post assignment 1

---
+++ static/assignments/extra.R
@@ -0,0 +1,24 @@
+# ## Importing Your Data into R
+# We will be covering this in detail next class, but for now, please do the following:
+#   
+#   #### Step 1. Know where your data is saved!
+#   This could be your Desktop, Box, Dropbox, etc. Just know where it is.
+# 
+# #### Step 2. In the Environment tab, click on `Import Dataset`
+# Choose **`From text (base)`**
+#   
+#   #### Step 3. Select the file
+#   You should get a pop-up like this:
+#   
+#   <center>
+#   <img src="../miscfigs/figure-html/popup.png" width = "70%">
+#   </center>
+#   
+#   
+#   #### Step 4. Make the following Changes:
+#   
+#   - In the top left box, name your data something that makes sense. For example: `hw1Data` or `votes_by_district` or whatever. This name *cannot* start with a number (e.g., `1hwData` would not work). 
+# - Last item on the left is a checkbox that says `Strings as factors`. Please **uncheck** that box. 
+# 
+# #### Step 5. Click "Import"
+# Once your data looks ok in that bottom window, go ahead and click "Import". You should now see your data.frame in the Environment tab. Email me ASAP if you are not able to get your data into R!
