331411154e351683835a24e3c8d168a9772adfc2
Shelly Cooper <shelly.cooper@wustl.edu>
2022-07-25 09:41:09 -0500

    make new quant orientation site

--- R/build.R
+++ R/build.R
@@ -9,9 +9,16 @@ slide_names_with_ext <- list.files(here::here("static/slides/"), pattern = "*.ht
 
 slide_names_sans_ext <- tools::file_path_sans_ext(slide_names_with_ext)
 
+# pdfize <- function(slide_name) {
+#   pagedown::chrome_print(here::here(paste0("static/slides/", slide_name, ".html")),
+#                          output = here::here(paste0("static/slides/", slide_name, ".pdf")))
+# }
+
 pdfize <- function(slide_name) {
   pagedown::chrome_print(here::here(paste0("static/slides/", slide_name, ".html")),
-                         output = here::here(paste0("static/slides/", slide_name, ".pdf")))
+                         output = here::here(paste0("static/slides/", slide_name, ".pdf")),
+                         options = list(printBackground = TRUE),
+                         wait = 10, timeout = 600)
 }
 
 sapply(slide_names_sans_ext, pdfize)
